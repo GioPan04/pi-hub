fe67fb91db26f81288929ed27a7b8932ef192a03
Gioele Pannetto <gioele@pannetto.com>
2024-04-24 16:30:38 +0200

    docs: added screenshot

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # pi_hub
 
+![screenshot](.github/screenshot.png)
+
 ## Todo
 
  - [ ] Home Assistant integration
--- lib/pages/screensaver.dart
+++ lib/pages/screensaver.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pi_hub/modules/background/widgets/random_background.dart';
-import 'package:pi_hub/modules/clock/providers/clock.dart';
 import 'package:pi_hub/modules/clock/widgets/big_clock.dart';
 
 @RoutePage(name: 'ScreensaverRoute')
@@ -11,8 +10,6 @@ class ScreensaverPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final time = ref.watch(clockProvider).valueOrNull ?? DateTime.now();
-
     return const Scaffold(
       body: SizedBox.expand(
         child: Stack(
